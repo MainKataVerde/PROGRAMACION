cd8198975c90c0527216cc3ec154887b237a537e
Oscar Mukeshimana Nzabarinda <101839576+MainKataVerde@users.noreply.github.com>
2023-10-11 13:32:13 +0200

    mejoramos el ejercico 1 con un control de la nota maxima y con mas comentarios

--- ifElse/src/main/ifelse.java
+++ ifElse/src/main/ifelse.java
@@ -2,20 +2,38 @@ package main;
 
 import java.util.Scanner;
 
+/*
+ * Programa basico que su funcion es la de obtener un numero del ususrio
+ * para que luego decir si ha aprobado el examen o no 
+ */
+
 public class ifelse {
 
 	public static void main(String[] args) {
+		//creamos la variables necesarias
+		int nt ;
+		//sacamos la clase Scanner para sacar el numero de los usuarios
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Itroduce la nota de tu examen");
-
-		int nt = sc.nextInt();
 		
-		if (nt>=60) {
-			System.out.println("Aprovado");
+		nt = sc.nextInt();
+		
+		/*
+		 * En este if solo tenemos que hacer que si el numero sea mayo
+		 * o igual que 60 el alumno haya aprovado 
+		 * pero por si ponen un numero moyor a la nota maxima posible no de resultado
+		 */
+		
+		if (nt>100) {
+			
+			System.out.println("Introduzca un numero dentro del rango de la nota");
 
+		}else if(nt>=60){
+			System.out.println("Aprovado");
 		}else {
 			System.out.println("Suspenso");
+
 		}
 	}
 
