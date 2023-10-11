7af81107f68e406c355858cf2a946dc2e9d07655
Oscar Mukeshimana Nzabarinda <101839576+MainKataVerde@users.noreply.github.com>
2023-10-11 14:31:41 +0200

    añadimos todos los comentario y mejoramos la composicion de cada ejercicio

--- ifElse/src/main/ifelse4.java
+++ ifElse/src/main/ifelse4.java
@@ -8,21 +8,26 @@ import java.util.Date;
 public class ifelse4 {
 
 	public static void main(String[] args) {
+		//variables
+		int dt , fc , res1 ;
 		Scanner sc = new Scanner(System.in);
 		//utilizamos la clase dateformat y simpledate fotma para sacar solo la fecha 
 		DateFormat dateFormat = new SimpleDateFormat("yyy");
-		
+		//guardamos el nuevo formato de fecha en una variable tipo sting
 		String date = dateFormat.format(new Date());
+		/*
+		 * date se guarda en una stinge pero como lo que queremos es el año solo
+		 * podemos pasar el valor de date a otra varibale y convertirla en int
+		 */
+		 dt = Integer.parseInt(date);
+		//sacamos la fecha de nacimiento del usuario
+		 fc = sc.nextInt();
 		
-		int dt = Integer.parseInt(date);
-		
-		int fc = sc.nextInt();
-		
-		int res1 = dt-fc;
-				
+		 res1 = dt-fc;
+		//si la fecha de naciemiento del usuario es mayo o igual que la actual se hace la operacion  		
 		if (fc<=dt) {
 			System.out.println(res1);
-		}else {
+		}else {//si no ocurre lo dicbo antes quieres decir que la fecha es futura asi que no ha nacido aun y no da resulatado
 			System.out.println("Fecha no valida");
 
 		}
