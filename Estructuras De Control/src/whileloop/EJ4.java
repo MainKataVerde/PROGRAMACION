1558ae5b87c8d0bc4946106b08aac9801ba6f784
Oscar Mukeshimana Nzabarinda <101839576+MainKataVerde@users.noreply.github.com>
2023-10-18 13:42:31 +0200

    añadimos el ejercicio 4 while

---
+++ Estructuras De Control/src/whileloop/EJ4.java
@@ -0,0 +1,38 @@
+package whileloop;
+
+import java.util.Scanner;
+
+public class EJ4 {
+
+	public static void main(String[] args) {
+		//VARIABLES
+		int num , al;
+		Scanner sc = new Scanner(System.in);
+		al = (int)(Math.random()*100-1+1)+1;
+		System.out.println(al);
+		System.out.println("Adivina un numero del 1 al 100");
+		System.out.print("Inserte el numeor que crees que es =");
+		num= sc.nextInt();
+		//NUMERO ALEATORIO
+		while(num!=al) {
+			if(num<0||num>100) {
+				System.out.print("Introduce un numero que entre dentro del rango = ");
+				num= sc.nextInt();
+			}else {
+				if(num>al) {
+					System.out.println("Te has pasado");
+					System.out.print("Prueba otra vez = ");
+					num= sc.nextInt();
+				}else if (num<al) {
+					System.out.println("Te has quedado corto");
+					System.out.print("Prueba otra vez = ");
+					num= sc.nextInt();
+				}
+			}
+		}
+		if (num == al) {
+			System.out.println("Has ganado muy bien!!!");
+		}
+	}
+
+}
