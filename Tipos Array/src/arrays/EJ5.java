a4f89d997e2520acda80bd128c42e93a035cd7c8
Oscar Nzabarinda Mukeshimana <101839576+MainKataVerde@users.noreply.github.com>
2023-11-10 17:03:31 +0100

    avance en el ejercicio 5 array

--- Tipos Array/src/arrays/EJ5.java
+++ Tipos Array/src/arrays/EJ5.java
@@ -1,10 +1,41 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class EJ5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		int buscar , alt ; 
+		
+		int mi_array[];
+		
+		mi_array = new int[11];
+		
+		System.out.print("Elige la casilla que quieras eliminar : ");
 
+		buscar = scanner.nextInt();
+		
+		System.out.print("Array original : ");
+		
+		for(int i = 0 ; i < mi_array.length;i++) {	
+			alt = (int)(Math.random()*10+1);
+			mi_array[i] = alt;
+			System.out.print(mi_array[i] + " ");
+		}
+		
+		System.out.print("\nDespues de eliminar el elemento "+mi_array[buscar]+" : ");
+		
+		for(int k = 0 ; k < mi_array.length; k++) {
+			System.out.print(mi_array[k] + " ");
+			
+			if(k == buscar) {
+				mi_array[k] = mi_array[k+1]; 
+			}
+		}
+		
 	}
+	
 
 }
